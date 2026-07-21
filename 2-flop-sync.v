0f243fd5d513fd248986f2a3803f27af6a3d82f4
Mayur Khairnar <mgkspit@gmail.com>
2026-07-21 18:49:30 +0530

    Update 2-flop-sync.v

--- 2-flop-sync.v
+++ 2-flop-sync.v
@@ -1,17 +1,21 @@
-module flop_2_sync
-  input data, clk, rst_n;
+module flop_2_sync (
+  input wire data, 
+  input wire clk, 
+  input wire rst_n,
+  output reg sync2);
+//Added sync2 as an output so the synchronized signal can be used outside the module
 
-  reg sync1, sync2;
+  reg sync1;
 
   always @(posedge clk or negedge rst_n)
-    begin if (!rst_n)
+    begin if (!rst_n) begin
       sync1 <= 1'b0;
       sync2 <= 1'b0;
-      else 
+    end
+      else begin
         sync1 <= data;
       sync2 <= sync1;
     end
-
-  
+    end
   
 endmodule
